f7790d38d7141dbec0558c0a25676c0e7aa6cbdb
Josue Sebastiao <josuesalazaku@gmail.com>
2023-08-27 14:30:44 +0200

    Clock-Analog

---
+++ scipt.js
@@ -0,0 +1,3 @@
+const secondsHands = document.getElementById('seconds-hands')
+const minutesHands = document.getElementById('minutes-hands')
+const hoursHands = document.getElementById('hours-hands')
